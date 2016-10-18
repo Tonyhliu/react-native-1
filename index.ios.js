c9bff9427ee6ab55874e1500f103729f9ad3910a
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-10-17 17:54:20 -0700

    button and textInput completed

--- index.ios.js
+++ index.ios.js
@@ -53,14 +53,80 @@
 // AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, View, Text, TextInput } from 'react-native';
+import Button from 'react-native-button';
 
-class HelloWorldApp extends Component {
+class SampleTextInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Placeholder',
+                    amount: null};
+  }
+
+  buttonClicked() {
+    // console.log("Button Clicked!");
+    let amount = parseInt(this.state.text) / 2;
+    this.setState({amount})
+    // console.log(amount);
+
+  }
+
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View>
+        <Text>Enter body weight!</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'green', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}/>
+        <Button style={{fontSize: 20, color: 'green'}}
+          styleDisabled={{color: 'red'}}
+          onPress={this.buttonClicked.bind(this)}>
+          Press me!
+        </Button>
+        <Text>
+        {this.state.amount === null ? '' : this.state.amount + "ounces of water a day"}
+        </Text>
+      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('HelloWorld', () => HelloWorldApp);
+AppRegistry.registerComponent('HelloWorld', () => SampleTextInput);
+
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, View } from 'react-native';
+//
+// class Blink extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {showText: true};
+//
+//     // Toggle the state every second
+//     setInterval(() => {
+//       this.setState({ showText: !this.state.showText });
+//     }, 1000);
+//   }
+//
+//   render() {
+//     let display = this.state.showText ? this.props.text : ' ';
+//     return (
+//       <Text>{display}</Text>
+//     );
+//   }
+// }
+//
+// class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Blink text='I love to blink' />
+//         <Blink text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' />
+//       </View>
+//     );
+//   }
+// }
+//
+// AppRegistry.registerComponent('HelloWorld', () => BlinkApp);
