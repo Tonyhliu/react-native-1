de4f73aafe9b97c23a90d728f55268661e246e23
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-10-18 15:50:35 -0700

    flex fixed

--- index.ios.js
+++ index.ios.js
@@ -52,6 +52,12 @@
 //
 // AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
 
+// Activity Level: Finally you will want to adjust that number
+// based on how often you work out, since you are expelling water
+// when you sweat. You should add 12 ounces of water to your daily
+// total for every 30 minutes that you work out. So if you work out
+// for 45 minutes daily, you would add 18 ounces of water to your daily intake.
+
 import React, { Component } from 'react';
 import { AppRegistry,
         View,
@@ -84,7 +90,7 @@ class SampleTextInput extends Component {
           <Text style={styles.toolbarTitle}>WaterBuddy</Text>
         </View>
         <View style={styles.secondContainer}>
-          <View>
+          <View style={{flex: 1}}>
             <Text style={styles.bwd}>Enter body weight!</Text>
             <TextInput style={styles.txtInput}
               onChangeText={(text) => this.setState({text})}
@@ -97,11 +103,11 @@ class SampleTextInput extends Component {
             </Button>
           </View>
         </View>
-        <View>
+        <View style={styles.result}>
           <Text style={styles.green}>
           {this.state.amount === null ? '' : this.state.amount + " " + "ounces of water a day"}
           {"\n"}
-          {this.state.amount === null ? '' : "or"}
+          {this.state.amount === null ? '' : "OR"}
           {"\n"}
           {this.state.amount === null ? '' : Math.round(this.state.amount * 28.35) + " " + "ml of water a day"}
           </Text>
@@ -112,6 +118,27 @@ class SampleTextInput extends Component {
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  toolbar: {
+    backgroundColor: '#a7d5f6',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  toolbarTitle: {
+    flex: 1,
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  result: {
+    flexDirection: 'row',
+    flex: .25,
+    justifyContent: 'center',
+    backgroundColor: 'lightgray'
+  },
   bwd: {
     color: 'black',
     fontWeight: 'bold',
@@ -119,13 +146,14 @@ const styles = StyleSheet.create({
     paddingTop: 50
   },
   secondContainer: {
-    flex: 1,
+    flex: .1,
     justifyContent: 'center',
     alignItems: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    backgroundColor: 'gray'
   },
   green: {
-    color: '#81c04d',
+    color: '#81c04d'
   },
   blue: {
     color: '#a7d5f6',
@@ -138,21 +166,6 @@ const styles = StyleSheet.create({
   btn: {
     fontSize: 20,
     color: '#a7d5f6'
-  },
-  toolbar: {
-    backgroundColor: '#a7d5f6',
-    paddingTop: 30,
-    paddingBottom: 10,
-    flexDirection: 'row'
-  },
-  toolbarTitle: {
-    color: '#fff',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    flex: 1
-  },
-  mainContainer: {
-
   }
 });
 
