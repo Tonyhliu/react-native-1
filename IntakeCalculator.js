99997cb03e289739e50034a4c1faca40a677f987
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-11-08 17:21:51 -0800

    modals added and styled

--- IntakeCalculator.js
+++ IntakeCalculator.js
@@ -14,8 +14,8 @@ import { View,
         StyleSheet
        } from 'react-native';
 
-// import Button from 'react-native-button';
 import { Button, CheckBox, Icon} from 'react-native-elements';
+// import Button from 'react-native-button';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 
 
@@ -44,10 +44,12 @@ export default class IntakeCalculator extends Component {
       text: '',
       amount: null,
       activity: 'none',
-      value: 90,
+      weight: 90,
       femaleChecked: false,
       maleChecked: false,
-      genderModalVisible: false
+      genderModalVisible: false,
+      weightModalVisible: false,
+      disclaimerModalVisible: false,
     };
   }
 
@@ -59,6 +61,14 @@ export default class IntakeCalculator extends Component {
 
   setGenderModal(visible) {
     this.setState({ genderModalVisible: visible});
+  }
+
+  setWeightModal(visible) {
+    this.setState({ weightModalVisible: visible});
+  }
+
+  setDisclaimerModal(visible) {
+    this.setState({ disclaimerModalVisible: visible});
   }
 
   buttonClicked() {
@@ -97,11 +107,48 @@ export default class IntakeCalculator extends Component {
       <ScrollView style={styles.mainContainer}>
 
         <View style={styles.firstContainer}>
+          <View style={{marginTop: 22}}>
+            <Modal
+              animationType={"fade"}
+              transparent={true}
+              visible={this.state.disclaimerModalVisible}
+              onRequestClose={() => {alert("Modal has been closed.");}}>
+            <View style={{justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: 'rgba(34, 34, 34, 0.50)'}}>
+             <View style={{backgroundColor: 'white', height: 275}}>
+                <Text style={{fontWeight: 'bold', textAlign: 'center', marginBottom: 10, marginTop: 10}}>DISCLAIMER</Text>
+                <Text style={{textAlign: 'center', marginBottom: 10}}>
+                Results provides an estimate of quantity of water intake needed per day based on weight & activity level and is not intended to give precise amounts.
+                </Text>
+                <Text style={{textAlign: 'center', marginBottom: 10}}>
+                  Also, note that 80% of estimated amount is met by consuming water & beverages, while the other 20% is derived from foods consumed.
+                </Text>
+
+                <Button title='CLOSE'
+                        onPress={() => {
+                          this.setDisclaimerModal(!this.state.disclaimerModalVisible);
+                        }} />
+              </View>
+             </View>
+            </Modal>
+          </View>
+
+
+
           <View style={styles.messageBox}>
             <Text style={styles.messageBoxTitle}>
               {'How much water should you drink?'.toUpperCase()}
             </Text>
             <Text style={styles.messageBoxBodyText}>{'Determine how much water you need using the calculator below'.toUpperCase()}</Text>
+
+            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 10}}>
+              <Text style={styles.bwd}>DISCLAIMER</Text>
+              <Icon
+                name='info-outline'
+                size={18}
+                iconStyle={{marginLeft: 5}}
+                onPress={() => {this.setDisclaimerModal(true);}}
+                />
+            </View>
           </View>
         </View>
 
@@ -109,16 +156,16 @@ export default class IntakeCalculator extends Component {
           <View style={styles.genderContainer}>
             <View style={{marginTop: 22}}>
               <Modal
-                animationType={"slide"}
-                transparent={false}
+                animationType={"fade"}
+                transparent={true}
                 visible={this.state.genderModalVisible}
                 onRequestClose={() => {alert("Modal has been closed.");}}
                 >
-               <View style={{marginTop: 22}}>
-                <View>
-                  <Text>Gender</Text>
-                  <Text>Generally, an adult male needs more water per day compared to an adult female due to higher producing sweat glands. In turn, generally an adult male needs 3 liters per day while an adult female needs about 2.2 liters per day. Some of this water will be through the foods you eat. </Text>
-                  <Text>Also, pregnant or breastfeeding women tend to require slightly more water than usual.</Text>
+              <View style={{justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: 'rgba(34, 34, 34, 0.50)'}}>
+               <View style={{backgroundColor: 'white', height: 275}}>
+                  <Text style={{fontWeight: 'bold', textAlign: 'center', marginBottom: 10, marginTop: 10}}>GENDER</Text>
+                  <Text style={{textAlign: 'center', marginBottom: 10}}>Generally, an adult male needs more water per day compared to an adult female due to higher producing sweat glands. In turn, generally an adult male needs 3 liters per day while an adult female needs about 2.2 liters per day. Some of this water will be through the foods you eat. </Text>
+                  <Text style={{textAlign: 'center', marginBottom: 10}}>Also, pregnant or breastfeeding women tend to require slightly more water than usual.</Text>
 
                     <Button title='CLOSE'
                             onPress={() => {
@@ -161,10 +208,43 @@ export default class IntakeCalculator extends Component {
           </View>
 
           <View style={styles.weightContainer}>
-            <Text style={styles.bwd}>WEIGHT</Text>
+            <View style={{marginTop: 22}}>
+              <Modal
+                animationType={"fade"}
+                transparent={true}
+                visible={this.state.weightModalVisible}
+                onRequestClose={() => {alert("Modal has been closed.");}}
+                >
+               <View style={{justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: 'rgba(34, 34, 34, 0.50)'}}>
+                <View style={{backgroundColor: 'white', height: 250}}>
+                  <Text style={{fontWeight: 'bold', textAlign: 'center', marginBottom: 10, marginTop: 10}}>WEIGHT</Text>
+                  <Text style={{textAlign: 'center', marginBottom: 10}}>
+                    Weight combined with Height provides us with your Body Mass Index or BMI.
+                    BMI is used in our calculation to factor in the surface area of your skin.
+                    The more surface area you have the greater amount of sweat you produce.
+                    Formula: weight / [height]^2
+                  </Text>
+                    <Button title='CLOSE'
+                            onPress={() => {
+                              this.setWeightModal(!this.state.weightModalVisible);
+                            }} />
+                </View>
+               </View>
+              </Modal>
+            </View>
+
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.bwd}>WEIGHT</Text>
+              <Icon
+                name='info'
+                size={18}
+                iconStyle={{marginLeft: 5, paddingBottom: 15}}
+                onPress={() => {this.setWeightModal(true);}}
+                />
+            </View>
 
             <Text style={styles.text}>
-              {this.state.value} pounds
+              {this.state.weight} pounds
             </Text>
             <Slider {...this.props}
                     minimumValue={90}
@@ -172,10 +252,18 @@ export default class IntakeCalculator extends Component {
                     step={1}
                     style={styles.sliderBar}
                     minimumTrackTintColor="#ffc123"
-                    onValueChange={(value) => this.setState({value: value})} />
+                    onValueChange={(val) => this.setState({weight: val})} />
           </View>
 
           <View style={styles.ageContainer}>
+
+          </View>
+
+          <View style={styles.HeightContainer}>
+
+          </View>
+
+          <View style={styles.activityContainer}>
 
           </View>
         </View>
@@ -197,8 +285,6 @@ export default class IntakeCalculator extends Component {
         </View>
 
         <View style={styles.quarterHeight}>
-          <Text>*NOTE: 80% of estimated amount is met by consuming water & beverages, while the other 20% is derived from foods consumed.</Text>
-          <Text>DISCLAIMER: Results provides an estimate of quantity of water intake needed per day based on weight & activity level and is not intended to give precise amounts.</Text>
           <Text style={styles.green}>
             {this.state.amount === null ? '' : this.state.amount + " " + "ounces of water a day"}
             {"\n"}
@@ -235,11 +321,7 @@ const styles = StyleSheet.create({
   genderContainer: {
     marginTop: 15,
     marginBottom: 20,
-    // flexDirection: 'row',
     alignItems: 'center'
-    // flex: 1
-    // height: 150,
-    // width: 300
   },
   weightContainer: {
     alignItems: 'center',
@@ -292,9 +374,9 @@ const styles = StyleSheet.create({
     paddingLeft:20,
     paddingRight:20,
     borderRadius:10,
-    width: 250,
+    width: 300,
     // marginBottom: 15,
-    // marginTop: 20
+    marginTop: 15
   },
   messageBoxTitle: {
     fontSize: 24,
